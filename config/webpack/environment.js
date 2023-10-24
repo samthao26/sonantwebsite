0a63a2d1539bf60a494b7898f64fc66ec286f39d
Sam Yen Thao <samthaobdt@gmail.com>
2023-10-24 10:05:51 +0700

    yolo

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,3 @@
-const { environment } = require('@rails/webpacker') // already present
-
-const webpack = require('webpack')
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-}))
+const { environment } = require('@rails/webpacker')
 
 module.exports = environment
